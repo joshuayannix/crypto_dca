0125a0570ac9bfda31fef01ec2671254060e9887
Josh <joshuayannix@gmail.com>
2021-05-19 23:36:27 -0400

    react table bootstrap output showing

--- src/PurchaseInstance.js
+++ src/PurchaseInstance.js
@@ -1,11 +1,11 @@
 import React from 'react';
+//import dayjs from "dayjs";
 
 const PurchaseInstance = ({ dollarAmountInvested, coinAmount, coinValue, date }) => {
-
+  
   return (
     <div>
-      Individual Purchase Instance!
-      {dollarAmountInvested}, {coinAmount}, {coinValue}, {date}
+      dollar amount invested:{dollarAmountInvested}, coinAmount: {coinAmount}, Crypto Purchase price: {coinValue}, date of purchase: {new Date(date).toLocaleDateString('en-US')}
     </div>
   )
 }
--- src/Show.js
+++ src/Show.js
@@ -8,6 +8,7 @@ import { useLocation } from 'react-router-dom';
 import queryString from "query-string";
 import axios from 'axios';
 import dayjs from "dayjs";
+import * as ReactBootStrap from 'react-bootstrap';
 
 function Show() {
 
@@ -19,11 +20,10 @@ function Show() {
   const [loading, setLoading] = useState(false);
   //const [error, setError] = useState(false)
   const [coinData, setCoinData] = useState([]);
-  const [dataArr, setDataArr] = useState([]);
 
   useEffect(() => {
     const params = queryString.parse(location.search);    
-    console.log('useEffect triggered from Show.js')
+    //console.log('useEffect triggered from Show.js')
 
     const validateValues = (params) => {
       const { amount, end, start, freq, coinType } = params;
@@ -59,14 +59,13 @@ function Show() {
 
     const range = `range?vs_currency=usd&from=${startDateUnix}&to=${endDateUnix}`;
     const url = `https://api.coingecko.com/api/v3/coins/${coinType}/market_chart/${range}`;
-    console.log('url:',url)
+    //console.log('url:',url)
     try {
       const response = await axios.get(url)
-      console.log('api response:',response.data)
       setCoinData(response.data.prices)
       
-      console.log('coinData after api call and setCoinData:', coinData);
-      setLoading(true);
+      //console.log('coinData after api call and setCoinData:', coinData);
+      //setLoading(true);
       initializeData()
     } catch(error) {
       console.log('Error from apiAxios', error)
@@ -74,24 +73,6 @@ function Show() {
   }
 
   const initializeData = () => {
-    let newData = []
-    let coinAmount = 0;
-    let dollarAmountInvested = 0;
-
-    //One month is 24 hrs * 30 days = 722 prices
-    for(let i=0; i<coinData.length; i+=722) {
-      dollarAmountInvested += Number(params.amount);
-      coinAmount += params.amount/coinData[i][1];
-
-      newData.push({
-        dollarAmountInvested,
-        coinAmount,
-        coinValue: coinData[i][1],
-        date: coinData[i][0]
-      })
-    }
-
-    setDataArr(newData)
     // set other states
     //console.log('newData:',newData)
     setLoading(false)
@@ -103,15 +84,12 @@ function Show() {
       <div>LOADING!</div>
     )
   }
-  let coinDataFromState = coinData
-  console.log('coinDataFromState', coinDataFromState)
-  let dataArrFromState = dataArr
-  console.log('dataArrFromState',dataArrFromState)
 
   const filteredData = [];
   let coinAmount = 0;
   let dollarAmountInvested = 0;
 
+  //One month is 24 hrs * 30 days = 722 prices
   for(let i=0; i<coinData.length; i+=722) {
       dollarAmountInvested += Number(params.amount);
       coinAmount += params.amount/coinData[i][1];
@@ -124,15 +102,34 @@ function Show() {
       })
   }
 
-  console.log('filteredData: ', filteredData)
-
+  const renderPurchase = (purchase, index) => {
+    return (
+      <tr key={index}>
+        <td>{purchase.dollarAmountInvested}</td>
+        <td>{purchase.coinAmount}</td>
+        <td>{purchase.coinValue}</td>
+        <td>{purchase.date}</td>
+      </tr>
+    )
+  }
 
   return (
     <div>
-      <h3>Show component</h3>
-      <div>coinDataFromState.length:{coinDataFromState.length}</div>
-      <div>dataArrFromState.length: {dataArrFromState.length}</div>
-      <div>filteredData.length: {filteredData.length}</div>
+      <h2>Show component</h2>
+      <div>
+        <h3>Table of Purchases</h3>
+        <ReactBootStrap.Table>
+          <thead>
+            <th>dollarAmountInvested</th>
+            <th>coin Amount</th>
+            <th>coin value</th>
+            <th>purchase date</th>
+          </thead>
+          <tbody>
+            {filteredData.map(renderPurchase)}
+          </tbody>
+        </ReactBootStrap.Table>
+      </div>
       <div>
         <h3>PurchaseInstance components from filteredData</h3>
         {filteredData.map(el => {
@@ -147,13 +144,6 @@ function Show() {
           
         })}
       </div>
-
-      
-
-      
-
-    
-
     </div>
   )
 }
